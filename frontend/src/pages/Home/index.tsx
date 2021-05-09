7ba54eff3e5d1da32eaa5628c7adad295a0515fd
Marcos Young <marcos.unitmu@gmail.com>
2021-05-09 17:59:08 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+           HAHAHAHAHA
+        </div>
+    );
+}
+
+export default Home;
